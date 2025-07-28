ee56c1ef49562b08a3f3cd3bcb2d385a7899bb62
Nivanown <32540777+nivanown@users.noreply.github.com>
2025-07-28 06:18:31 +0500

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -30,7 +30,7 @@ Timer.prototype={
 function tmr(){
 
  // Set countdown limit
- var tl = new Date('2024/10/29 20:00:00');
+ var tl = new Date('2025/11/29 20:00:00');
 
  // You can add time's up message here
  var timer = new Timer('tmr',tl,'<span class="number-wrapper"><div class="line"></div><span class="number end">Time is up!</span></span>');
